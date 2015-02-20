650f7b8b4238739e3862fbaff8e21fe857009d8d
katzhang-ias <arcadychang@gmail.com>
2015-02-19 22:24:03 -0500

    addec crypto and secretz

--- combiner.js
+++ combiner.js
@@ -6,27 +6,26 @@ var zlib = require('zlib');
 module.exports = function() {
 	var grouping = through(write, end), current;
 
-	function write(line) {
-		var row = JSON.parse(line);
-
-		if (row.type === 'genre') {
-			if (current) {
-				this.queue(JSON.stringify(current) + '\n');
-			}
-
-			current = { name: row.name, books: []};
-
-		} else if (row.type === 'book') {
-			current.books.push(row.name);
-		}
-	}
-
-	function end() {
-		if (current) {
-			this.queue(JSON.stringify(current) + '\n');
-		}
-		this.queue(null);
-	}
+    function write (line) {
+        if (line.length === 0) return;
+        var row = JSON.parse(line);
+        
+        if (row.type === 'genre') {
+            if (current) {
+                this.queue(JSON.stringify(current) + '\n');
+            }
+            current = { name: row.name, books: [] };
+        }
+        else if (row.type === 'book') {
+            current.books.push(row.name);
+        }
+    }
+    function end () {
+        if (current) {
+            this.queue(JSON.stringify(current) + '\n');
+        }
+        this.queue(null);
+    }
 	return combine(
 		split(),
 		grouping,
---
+++ secretz.js
@@ -0,0 +1,23 @@
+var crypto = require('crypto');
+var zlib = require('zlib');
+var tar = require('tar');
+var through = require('through');
+
+var decipher = crypto.createDecipher(process.argv[2], process.argv[3]);
+
+var parser = tar.Parse();
+
+parser.on('entry', function(e) {
+	if (e.type !== 'File') {
+		return;
+	}
+
+	var hash = crypto.createHash('md5', {encoding: 'hex'});
+	e.pipe(hash).pipe(through(null, end)).pipe(process.stdout);
+	function end() {
+		this.queue(' ' + e.path + '\n');
+	}
+});
+
+process.stdin.pipe(decipher).pipe(zlib.createGunzip()).pipe(parser);
+
